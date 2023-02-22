25ae780b7e7a0168c3e1b0812b3c7f3bd6038480
shakti goyal <shaktigoyal@192.168.1.5>
2023-02-22 18:50:12 +0530

    error fixed

--- index.js
+++ index.js
@@ -44,46 +44,49 @@ app.get('/bolobhai', (req,res)=>{
 
 app.post('/bolobhai', async (req,res)=>{
 
-
-  const user_id = req.body.entry[0].id;
-  const user_comment_id = req.body.entry[0].changes[0].value.comment_id;
-  const user_media_id = req.body.entry[0].changes[0].value.media_id;
-  const accessToken = 'EAAKf7JXO2hwBAHqQuU4INLEIi1sMGuTyVGBrnkVfsWzkjTx6BVTVwwWoIdNm0ymJcSwLF5NpEmTDXnFBRAuZAhUmE6tODvUrL4Q2rgitjudE85FZAAXVsLBQ8EFuPbqUKZBRiKwgq7mOwBVfwD1cOvIr3aOPeLZC9yNG71Vid9Dt62e7DuhHu3v1HUWntQakZBloSAJ3qUgZDZD';
-
-
-  const api_url = `https://graph.facebook.com/${user_id}?fields=mentioned_comment.comment_id(${user_comment_id}){media{id,media_url}}&access_token=${accessToken}`;
-
-  const request = await axios.get(api_url);
-
-  const mediaUrl = request.data.mentioned_comment.media.media_url;
-
-  const second_api_url = `https://sasssycomment.cognitiveservices.azure.com/vision/v3.1/describe?maxCandidates=1`;
-
-  const second_request = await axios.post(second_api_url, {url: mediaUrl}, {headers: {'Ocp-Apim-Subscription-Key': '90c0876eb13c46438d69ca3e566d5b5c'}});
-
-
-  const image_caption = second_request.data.description.captions[0].text;
-
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `give a sassy compliment on a photo which describes '${image_caption}'`,
-    temperature: 0,
-    max_tokens: 256
-  });
-
-  const compiment = response.data.choices[0].text;
-
-  const third_api_url = `https://graph.facebook.com/${user_id}/mentions`;
-
-
-  await axios.post(third_api_url, {
-    comment_id: user_comment_id,
-    media_id: user_media_id,
-    message: compiment,
-    access_token: accessToken
-  });
-
-  res.sendStatus(200);
+   try{
+    const user_id = req.body.entry[0].id;
+    const user_comment_id = req.body.entry[0].changes[0].value.comment_id;
+    const user_media_id = req.body.entry[0].changes[0].value.media_id;
+    const accessToken = 'EAAKf7JXO2hwBAHqQuU4INLEIi1sMGuTyVGBrnkVfsWzkjTx6BVTVwwWoIdNm0ymJcSwLF5NpEmTDXnFBRAuZAhUmE6tODvUrL4Q2rgitjudE85FZAAXVsLBQ8EFuPbqUKZBRiKwgq7mOwBVfwD1cOvIr3aOPeLZC9yNG71Vid9Dt62e7DuhHu3v1HUWntQakZBloSAJ3qUgZDZD';
+  
+  
+    const api_url = `https://graph.facebook.com/${user_id}?fields=mentioned_comment.comment_id(${user_comment_id}){media{id,media_url}}&access_token=${accessToken}`;
+  
+    const request = await axios.get(api_url);
+  
+    const mediaUrl = request.data.mentioned_comment.media.media_url;
+  
+    const second_api_url = `https://sasssycomment.cognitiveservices.azure.com/vision/v3.1/describe?maxCandidates=1`;
+  
+    const second_request = await axios.post(second_api_url, {url: mediaUrl}, {headers: {'Ocp-Apim-Subscription-Key': '90c0876eb13c46438d69ca3e566d5b5c'}});
+  
+  
+    const image_caption = second_request.data.description.captions[0].text;
+  
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `give a sassy compliment on a photo which describes '${image_caption}'`,
+      temperature: 0,
+      max_tokens: 256
+    });
+  
+    const compiment = response.data.choices[0].text;
+  
+    const third_api_url = `https://graph.facebook.com/${user_id}/mentions`;
+  
+  
+    await axios.post(third_api_url, {
+      comment_id: user_comment_id,
+      media_id: user_media_id,
+      message: compiment,
+      access_token: accessToken
+    });
+  
+    res.sendStatus(200);
+   } catch(e){
+     console.log(e);
+   }
 
   
 });
