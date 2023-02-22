ffeb99f65af52ecf92e30a9ffe2aaa1f82766160
shakti goyal <shaktigoyal@192.168.1.5>
2023-02-22 19:10:33 +0530

    new update

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5001; 
 
-const configuration = new Configuration({
-  apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
+
 
 app.get('/', (req,res)=>{
   res.send({status: "hello world"});
@@ -44,25 +41,30 @@ app.get('/bolobhai', (req,res)=>{
 
 app.post('/bolobhai', async (req,res)=>{
 
+  const configuration = new Configuration({
+    apiKey: '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',
+  });
+  const openai = new OpenAIApi(configuration);
 
-  const user_id = req.body.entry[0].id;
-  const user_comment_id = req.body.entry[0].changes[0].value.comment_id;
-  const user_media_id = req.body.entry[0].changes[0].value.media_id;
-  const accessToken = 'EAAKf7JXO2hwBANCkS8eD4IgEbWDh4R1aWVAVkTV1m9qOdZAGwlstaqfknNRD5uCPZAQIFUR6caKnYSwyHC1PPPQCcL3WysFyLiiwCQ3Sn7lZCcNrswIcY1Ottxba1LfA3VRtIaIv3Ux875PL613fFrPauvBy3qZBfYcl0wtCnMXuO5dn69uZAQCuQYrkQh60bcGUOeADJBAAgTmVe5nft';
+
+  const user_id = await req.body.entry[0].id;
+  const user_comment_id = await req.body.entry[0].changes[0].value.comment_id;
+  const user_media_id =  await req.body.entry[0].changes[0].value.media_id;
+  const accessToken =  'EAAKf7JXO2hwBANCkS8eD4IgEbWDh4R1aWVAVkTV1m9qOdZAGwlstaqfknNRD5uCPZAQIFUR6caKnYSwyHC1PPPQCcL3WysFyLiiwCQ3Sn7lZCcNrswIcY1Ottxba1LfA3VRtIaIv3Ux875PL613fFrPauvBy3qZBfYcl0wtCnMXuO5dn69uZAQCuQYrkQh60bcGUOeADJBAAgTmVe5nft';
 
 
   const api_url = `https://graph.facebook.com/${user_id}?fields=mentioned_comment.comment_id(${user_comment_id}){media{id,media_url}}&access_token=${accessToken}`;
 
   const request = await axios.get(api_url);
 
-  const mediaUrl = request.data.mentioned_comment.media.media_url;
+  const mediaUrl = await request.data.mentioned_comment.media.media_url;
 
   const second_api_url = `https://sasssycomment.cognitiveservices.azure.com/vision/v3.1/describe?maxCandidates=1`;
 
   const second_request = await axios.post(second_api_url, {url: mediaUrl}, {headers: {'Ocp-Apim-Subscription-Key': '90c0876eb13c46438d69ca3e566d5b5c'}});
 
 
-  const image_caption = second_request.data.description.captions[0].text;
+  const image_caption =  await second_request.data.description.captions[0].text;
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
